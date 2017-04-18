398457bca3453969e3b3fed735444e0c24acbfeb
cz1 <develop.cz1@gmail.com>
2017-04-18 11:55:20 +0800

    Configuration deployment

--- src/main/java/com/cz1/WkServerApplication.java
+++ src/main/java/com/cz1/WkServerApplication.java
@@ -2,10 +2,20 @@ package com.cz1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WkServerApplication {
-	public static void main(String[] args) {
+public class WkServerApplication extends SpringBootServletInitializer {
+
+	// run with war
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WkServerApplication.class);
+	}
+
+	// run with jar
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(WkServerApplication.class, args);
 	}
 }
